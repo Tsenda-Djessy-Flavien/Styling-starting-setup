87e1bf78f56ab796011ebf9821350f6ffe669bfa
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-15 11:17:56 +0100

    Classes et propriétés calculées

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ const app = createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+  },
   methods: {
     boxSelected(box) {
-        if (box === 'A') {
-            // this.boxASelected = true; // no toggle
-            this.boxASelected = !this.boxASelected; // toggle
-        } else if (box === 'B'){
-            this.boxBSelected = !this.boxBSelected;
-        } else {
-            this.boxCSelected = !this.boxCSelected;
-        }
-    }
+      if (box === "A") {
+        // this.boxASelected = true; // no toggle
+        this.boxASelected = !this.boxASelected; // toggle
+      } else if (box === "B") {
+        this.boxBSelected = !this.boxBSelected;
+      } else {
+        this.boxCSelected = !this.boxCSelected;
+      }
+    },
   },
 });
 
